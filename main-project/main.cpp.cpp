7c1d239aa5cac77544e954cfa8ea2b445d80349d
zgut388-byte <zgut388@gmail.com>
2025-12-19 09:04:37 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp.cpp
+++ main-project/main.cpp.cpp
@@ -7,7 +7,7 @@ int main()
 {
     cout << "Вариант 5-6" << endl;
     cout << "Задание: cpp-console-library-subscription" << endl;
-    cout << "Выполнил: Каннан Алан << endl;
+    cout << "Выполнил: Каннан Алан" << endl;
     cout << "Hello, world!" << endl;
     return 0;
 }
